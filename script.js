1a8b7c9ad4863409f02b415d8d997e404a95bbb9
jonasfabricio <jonas05fabrsousa@gmail.com>
2024-07-10 20:59:53 -0300

    Correção de bugs e novas funções.

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ let white = true
 let paragraph = document.getElementsByClassName('paragraph')
 let inputs = document.getElementsByClassName('inputs')
 let button = document.getElementById('button')
+let endTittle = document.getElementById('endTittle')
+let endparagraph = document.getElementById('endParagraph')
+let buttonPrevious = document.getElementById('buttonPrevious')
 
 function toggle(){
     white = !white
@@ -31,6 +34,8 @@ function toggle(){
         for (let i = 0; i < 5; i++) {
             inputs[i].style.color = 'black';
         }
+        endParagraph.style.color='black'
+        endTittle.style.color='black'
         button.style.border='0px'
         turnWiteOrBlack.style.backgroundColor='black'
     }else{
@@ -47,6 +52,9 @@ function toggle(){
         for (let i = 0; i<5; i++){
             inputs[i].style.color='white'
         }
+        endParagraph.style.color='white'
+        endTittle.style.color='white'
+        buttonPrevious.style.border='1px solid white'
         button.style.border='1px solid white'
         turnWiteOrBlack.style.backgroundColor='rgba(255, 255, 255, 0.500)'
     }
@@ -80,7 +88,7 @@ month.addEventListener("keyup", function() {
     if (monthn >= 0 && monthn <= 12){
         cardDate.innerText = `${monthn}/${year.value}`
     }else{
-        monthInvalid()
+        monthInvalid()  
     }
 })
 
@@ -93,6 +101,11 @@ month.addEventListener("blur", function monthInvalid(){
 })
 
 year.addEventListener("keyup", function(){
+    /*  
+    if(year.value < 10){
+        return 0 + year.value
+    }
+    */
     cardDate.innerText = `${month.value}/${year.value}`
 })
 
@@ -103,9 +116,13 @@ cvc.addEventListener("keyup", function(){
 })
 
 function confirm(){
-    let informations = document.getElementById('informations')
-    informations.style.display='none'
-    end.style.display='block'
+    if(year.value != 0 && cvc.value != 0 && month.value != 0 && number.value != 0 && name.value != 0){
+        let informations = document.getElementById('informations')
+        informations.style.display='none'
+        end.style.display='block'
+    }else{
+        alert('Prencha tudo abaixo!')
+    }
 }
 
 function previous(){
